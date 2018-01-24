574111a85993f28841a26a533f310451dd92365f
Manpreet Singh <manpreet414@gmail.com>
2018-01-24 12:31:20 -0400

    online db connection

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@
 import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 //Check environment development or production
-// mongoose.connect('mongodb://localhost:27017/iamLVC');
+mongoose.connect('mongodb://admin:admin@ds111638.mlab.com:11638/iamlvc');
 
 //check if we are connected successfully or not
 const db = mongoose.connection;
